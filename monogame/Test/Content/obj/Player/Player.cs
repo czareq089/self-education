5477fd47b1091bc44008855daa5096a4ff8c4f41
Cezary Kotlarski <cezary.kotlarski@gmail.com>
2024-03-19 07:35:11 +0100

    dodanie wszystkiego

---
+++ monogame/Test/Content/obj/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Test.Content.obj.Player;
+
+public class Player
+{
+    public Texture2D Texture { get; set; }
+    public Vector2 Position { get; set; }
+    
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, Position, Color.White);
+    }
+    
+}
